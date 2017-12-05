7f11069b1963b8ee01765e36372927cedcca9c92
John Vincent Uy Calacsan <johnvuc@hotmail.com>
2017-12-05 15:12:18 +0100

    Local Weather App

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// API GET Location
+function currentLocation() {
+
+	var URL = "http://ip-api.com/json";
+	$.get(URL, function(data) {
+	var lat = data.lat;
+	var lon = data.lon;
+	localweather(lat, lon);
+	});
+}
+
+// INTERACT WITH THE WEATHER API
+function localweather(lat,lon) {
+	var APIKEY = "f3e227a4fcd958200be8fe9d55f5ca88"
+	var APIURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+APIKEY+"&units=metric";
+	
+	$.get(APIURL, function(data){
+
+	$('#location').append(data.main.temp+" &#176;C");
+	$('#temp').append(data.name+", "+data.sys.country);
+	$('#sky').append(data.weather[0].main);
+	$('#img').attr("src", "http://openweathermap.org/img/w/"+data.weather[0].icon +".png");
+	//console.log(data.weather[0].main);
+	});
+}
+currentLocation();
+
+
+
